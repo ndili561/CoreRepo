45b1b98ce9174d7b4c8a34ff16d7094d32c94ae7
Dilillon <nicola.dilillo@icloud.com>
2018-02-26 11:53:01 +0000

    Add project files.

---
+++ Repository/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public interface IEntity
+    {
+        DateTime CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime ModifiedDate { get; set; }
+        string ModifiedBy { get; set; }
+    }
+}
